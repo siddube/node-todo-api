982496c66aa8355a16b505ed713822067ba43b2f
Prajwal Belagatti <prajwal.be.21@gmail.com>
2018-05-27 14:31:28 +0530

    Test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -92,3 +92,37 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('Should delete individual id', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${todos[1]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    }).end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      Todo.findById(hexId).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+  
+  it('Should send 404 for no id found', (done) => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+  
+  it('Should send 404 for non object ids', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
