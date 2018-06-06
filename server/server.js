3acb2251f853a26918974c1bd7b202b9ffa25b04
Prajwal Belagatti <prajwal.be.21@gmail.com>
2018-06-06 17:45:40 +0530

    Add DELETE users/me/token

--- server/server.js
+++ server/server.js
@@ -113,6 +113,15 @@ app.post('/users/login', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, (e) => {
+    res.status(401).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`App running on ${port}`);
 });
